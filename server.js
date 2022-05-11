5ea72fb7118fc7026c0bf152c6417e7c0c014207
NTL7D <nguyenthanhlong528@gmail.com>
2022-05-11 15:35:34 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const app = express();
+app.use(morgan('tiny'));
+app.use(bodyparser.urlencoded({extended:true}))
+app.set("view engine","ejs")
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.get('/',(req,res)=>{
+    res.send("Crud Final");
+})
+app.listen(3005, ()=>{console.log('Server is running on http://localhost:3005')});
+
+
