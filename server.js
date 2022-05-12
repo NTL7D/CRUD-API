8b48f9a83cc312b51dab9663d2b0cbd5da9a5b3f
NTL7D <nguyenthanhlong528@gmail.com>
2022-05-12 11:42:23 +0700

    update 5

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyparser = require("body-parser");
-const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
-app.use(morgan('tiny'));
-app.use(bodyparser.urlencoded({extended:true}))
-app.set("view engine","ejs")
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
-app.get('/',(req,res)=>{
-    res.send("Crud Final");
-})
-app.listen(3005, ()=>{console.log('Server is running on http://localhost:3005')});
 
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8000;
 
+app.use(morgan("tiny"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs"));
+
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/images", express.static(path.resolve(__dirname, "assets/images")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/add_product", (req, res) => {
+  res.render("add_product");
+});
+
+app.get("/edit_product", (req, res) => {
+  res.render("edit_product");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
